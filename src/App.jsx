54154d4067475c1c347b82d8a4a0c9b30deb5180
alfieqashwa <alfieqahwa@gmail.com>
2018-11-19 22:16:48 +0700

    finished Writing your first React GraphQL Client

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,54 @@ import axios from 'axios';
 const TITLE = 'React GraphQL GitHub Client';
 
 const axiosGitHubGraphQL = axios.create({
-  baseURL: 'https://api.github.com/graphql'
+  baseURL: 'https://api.github.com/graphql',
   headers : {
-    Authorization: 'bearer YOUR_GITHUB_PERSONAL_ACCESS_TOKEN',
+    Authorization: `bearer ${
+      process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN // TOKEN is hidden on .env
+    }`,
   },
-})
+});
 
 class App extends Component {
+  state = {
+    path: 'the-road-to-learn-react/the-road-to-learn-react',
+  };
+
+  componentDidMount() {
+    // fetch data
+  }
+
+  onChange = (e) => {
+    this.setState({ path: e.target.value });
+  }
+
+  onSubmit = (e) => {
+    // fetch data
+    e.preventDefault();
+  };
+
   render() {
+    const { path } = this.state;    // destructuring
     return (
       <div className="App">
         <h1>{TITLE}</h1>
+
+        <form onSubmit={this.onSubmit}>
+          <label htmlFor="url">
+            Show open issues for https://github.com/
+          </label>
+          <input
+            id="url"
+            type="text"
+            value={path}
+            onChange={this.onChange}
+            style={{ width: '300px' }}
+          />
+          <button type="submit">Search</button>
+        </form>
+        <hr />
+
+        {/*...result!*/}
       </div>
     );
   }
